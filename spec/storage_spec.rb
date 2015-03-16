d2091bc8e5b4197d1fbfc7aa41aa1937adbb1e8e
Saimon Lovell (from Zen) <staysynchronize@gmail.com>
2015-03-16 18:28:06 -0400

    CentOS storage spec update

--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -15,15 +15,15 @@ describe 'With storage info' do
     end
 
     it 'will have valid size' do
-      expect(@storage.first.last[:size]).to match /(nil|\d+(T|G|M|K))/
+      expect(@storage.first.last[:size]).to match /(nil|\d+)(T|G|M|K)?/
     end
 
     it 'will have valid used' do
-      expect(@storage.first.last[:used]).to match /(nil|\d+(T|G|M|K))/
+      expect(@storage.first.last[:used]).to match /(nil|\d+)(T|G|M|K)?/
     end
 
     it 'will have valid available' do
-      expect(@storage.first.last[:available]).to match /(nil|\d+(T|G|M|K))/
+      expect(@storage.first.last[:available]).to match /(nil|\d+)(T|G|M|K)?/
     end
 
     it 'will have valid used_percentage' do
